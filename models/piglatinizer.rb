749e35dd6ca2d12a470e6171708f184497d2e524
Ally Netzel <netzelally@gmail.com>
2018-06-01 01:54:30 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,29 +1,29 @@
-require "pry"
 class PigLatinizer
   attr_reader :word
 
   def initialize
     @word = word
   end
+  def piglatinize(word)
+    word_array = word.split("")
+    vowels = /\b[aeiouAEIOU]/
+    consonants_3 = /\A(?i:(?![aeiou])[a-z]){3}/
+    consonants_2 = /\A(?i:(?![aeiou])[a-z]){2}/
+    consonant_1 = /\A(?i:(?![aeiou])[a-z]){1}/
+    result = []
 
-def piglatinize(word)
-  word_array = word.split("")
-  vowels = /[aeiou]/
-  consonants_3 = /\A(?i:(?![aeiou])[a-z]){3}/
-  consonants_2 = /\A(?i:(?![aeiou])[a-z]){2}/
-  consonant_1 = /[^aeiou][a-z]{1}/
-  result = []
-
-  if word.match(vowels) != word_array[0]
-    binding.pry
-    result = word_array + ["way"]
-  elsif word.scan(consonant_1)
-    result = [word_array[1..word_array.length-1]] + [word_array[0]] + ["ay"]
-  elsif word.scan(consonants_2)
-    result = [word_array[2..word_array.length-1]] + [word_array[0..1]] + ["ay"]
-  else
-    result = [word_array[3..word_array.length-1]] + [word_array[0..2]] + ["ay"]
+    if word_array[0].scan(vowels) != []
+      result = [word_array[0..word_array.length-1]] + ["way"]
+    end
+    if word.scan(consonant_1) != []
+      result = [word_array[1..word_array.length-1]] + [word_array[0]] + ["ay"]
+    end
+    if word.scan(consonants_2) != []
+      result = [word_array[2..word_array.length-1]] + [word_array[0..1]] + ["ay"]
+    end
+    if word.scan(consonants_3) != []
+      result = [word_array[3..word_array.length-1]] + [word_array[0..2]] + ["ay"]
+    end
+    return result.join("")
   end
-  return result.join("")
-end
 end
